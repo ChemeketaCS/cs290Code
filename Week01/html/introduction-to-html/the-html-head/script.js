798279aae837a27c3440934be8d12181f1fcaad7
Andrew Scholer <ascholer@chemeketa.edu>
2021-04-04 13:15:30 -0700

    Week01 mad prettier

--- Week01/html/introduction-to-html/the-html-head/script.js
+++ Week01/html/introduction-to-html/the-html-head/script.js
@@ -1,21 +1,22 @@
-const list = document.createElement('ul');
-const info = document.createElement('p');
-const html = document.querySelector('html');
+const list = document.createElement("ul");
+const info = document.createElement("p");
+const html = document.querySelector("html");
 
-info.textContent = 'Below is a dynamic list. Click anywhere outside the list to add a new list item. Click an existing list item to change its text to something else.';
+info.textContent =
+  "Below is a dynamic list. Click anywhere outside the list to add a new list item. Click an existing list item to change its text to something else.";
 
 document.body.appendChild(info);
 document.body.appendChild(list);
 
-html.onclick = function() {
-  const listItem = document.createElement('li');
-  const listContent = prompt('What content do you want the list item to have?');
+html.onclick = function () {
+  const listItem = document.createElement("li");
+  const listContent = prompt("What content do you want the list item to have?");
   listItem.textContent = listContent;
   list.appendChild(listItem);
 
-  listItem.onclick = function(e) {
+  listItem.onclick = function (e) {
     e.stopPropagation();
-    const listContent = prompt('Enter new content for your list item');
+    const listContent = prompt("Enter new content for your list item");
     this.textContent = listContent;
-  }
-}
+  };
+};
