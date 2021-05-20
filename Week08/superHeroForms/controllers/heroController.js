076eaf485d2c2363a4e90009c1f6ee1a88f3d8b0
Andrew Scholer <ascholer@chemeketa.edu>
2021-05-20 08:43:23 -0700

    Add express validator sample to Week08 Superhero

--- Week08/superHeroForms/controllers/heroController.js
+++ Week08/superHeroForms/controllers/heroController.js
@@ -88,74 +88,83 @@ exports.update_get = async function (req, res, next) {
   }
 };
 
+//Use express-validator to remove harmful content
+const { body } = require("express-validator");
+
 //Handles submission of the form
-exports.update_post = async function (req, res, next) {
-  try {
-    //If team exists in DB, fetch it
-    let hero = await Hero.findById(req.params.id).exec();
-    //If not, make one
-    if (hero === null)
-      hero = new Hero({
-        _id: req.body.id,
-      });
-
-    console.log(req.body);
-
-    //Process Powers Input - split string into an array
-    let powersList = req.body.powers.split("\n");
-    for (let i = 0; i < powersList.length; i++) {
-      powersList[i] = powersList[i].trim(); //get rid of extra whitespace at begin/end of each
-      if (powersList[i] === "") {
-        //Item is empty, so delete it
-        powersList.splice(i, 1);
-        i--;
-      }
-    }
-
-    //Replace existing data
-    hero.name = req.body.name;
-    hero.secretIdentity = req.body.secretIdentity;
-    hero.age = req.body.age;
-    //If team is not empty, use that value, otherwise clear it out
-    hero.team = req.body.team !== "" ? req.body.team : undefined;
-    hero.powers = powersList;
-
-    //Just print some of the "fake" team info to console
-    //----------------------------------------------------------
-    //Get from select list:
-    console.log("You fake selected");
-    console.log(req.body.teamFake);
-    //Get from checkboxes - will be string if one, array if multiple
-    //So first force into an array. Concat will smash either the string or
-    // array elements into an empty array
-    let fakeData2 = [].concat(req.body.teamFake2);
-    console.log("You fake selected");
-    console.log(fakeData2);
-    //Get from multiselect select list - same idea as checkboxes
-    let fakeData3 = [].concat(req.body.teamFake3);
-    console.log("You fake selected");
-    console.log(fakeData3);
-    //----------------------------------------------------------
-
-    let teams = await Team.find().select("squadName").exec();
-    //Try to save it
-    hero
-      .save()
-      .then((hero) => {
-        //Success, redirect to details view of hero
-        res.redirect(hero.url);
-      })
-      .catch((err) => {
-        //Problem, show the form with error messages
-        console.log(err.message);
-        res.render("superheroForm.ejs", {
-          title: `Update ${hero.name}`,
-          hero: hero,
-          teams: teams,
-          errors: routeHelper.errorParser(err.message),
+exports.update_post = [
+  //First HTML escape all the text inputs
+  // body("name").escape(),
+  // body("secretIdentity").escape(),
+  // body("powers").escape(),
+  async function (req, res, next) {
+    try {
+      //If team exists in DB, fetch it
+      let hero = await Hero.findById(req.params.id).exec();
+      //If not, make one
+      if (hero === null)
+        hero = new Hero({
+          _id: req.body.id,
         });
-      });
-  } catch (err) {
-    next(err);
-  }
-};
+
+      console.log(req.body);
+
+      //Process Powers Input - split string into an array
+      let powersList = req.body.powers.split("\n");
+      for (let i = 0; i < powersList.length; i++) {
+        powersList[i] = powersList[i].trim(); //get rid of extra whitespace at begin/end of each
+        if (powersList[i] === "") {
+          //Item is empty, so delete it
+          powersList.splice(i, 1);
+          i--;
+        }
+      }
+
+      //Replace existing data
+      hero.name = req.body.name;
+      hero.secretIdentity = req.body.secretIdentity;
+      hero.age = req.body.age;
+      //If team is not empty, use that value, otherwise clear it out
+      hero.team = req.body.team !== "" ? req.body.team : undefined;
+      hero.powers = powersList;
+
+      //Just print some of the "fake" team info to console
+      //----------------------------------------------------------
+      //Get from select list:
+      console.log("You fake selected");
+      console.log(req.body.teamFake);
+      //Get from checkboxes - will be string if one, array if multiple
+      //So first force into an array. Concat will smash either the string or
+      // array elements into an empty array
+      let fakeData2 = [].concat(req.body.teamFake2);
+      console.log("You fake selected");
+      console.log(fakeData2);
+      //Get from multiselect select list - same idea as checkboxes
+      let fakeData3 = [].concat(req.body.teamFake3);
+      console.log("You fake selected");
+      console.log(fakeData3);
+      //----------------------------------------------------------
+
+      let teams = await Team.find().select("squadName").exec();
+      //Try to save it
+      hero
+        .save()
+        .then((hero) => {
+          //Success, redirect to details view of hero
+          res.redirect(hero.url);
+        })
+        .catch((err) => {
+          //Problem, show the form with error messages
+          console.log(err.message);
+          res.render("superheroForm.ejs", {
+            title: `Update ${hero.name}`,
+            hero: hero,
+            teams: teams,
+            errors: routeHelper.errorParser(err.message),
+          });
+        });
+    } catch (err) {
+      next(err);
+    }
+  },
+];
