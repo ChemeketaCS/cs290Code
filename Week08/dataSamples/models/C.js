888ff6ba5649cdc2ba02df6c9eb3d8a47e05721d
Andrew Scholer <ascholer@chemeketa.edu>
2022-05-16 12:57:11 -0700

    Remove unused samples from week 8. Break validation out into seperate folder

--- Week08/dataSamples/models/C.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-var CSchema = new Schema({
-  name: { type: String },
-});
-
-//Export model
-module.exports = mongoose.model("C", CSchema);
