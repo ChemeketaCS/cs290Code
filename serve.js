357933c26ad3b8ea1f71fbe7d946419152203b0f
Andrew Scholer <ascholer@chemeketa.edu>
2022-05-23 13:18:00 -0700

    Update root level web server

--- serve.js
+++ serve.js
@@ -5,7 +5,6 @@ const port = 3000;
 var serveIndex = require("serve-index");
 
 app.use("/", express.static("."), serveIndex(__dirname + "/"));
-//app.use(express.static("."));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
