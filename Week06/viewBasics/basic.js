9a32be9d4fdcca7e6c391e4bab0ac25005a53ef4
Andrew Scholer <ascholer@chemeketa.edu>
2022-05-30 10:31:58 -0700

    Cleanup

--- Week06/viewBasics/basic.js
+++ Week06/viewBasics/basic.js
@@ -5,10 +5,20 @@ ejs.renderFile(
   "views/basic.ejs",
   {
     name: "Andrew <b>Scholer</b>",
-    //age: 47,
+    age: 47,
   },
   function (err, result) {
     if (err) console.log("ERROR - " + err);
     console.log(result);
   }
 );
+
+//List I want to pass to template
+let myList = ["Thing one", "Thing two"];
+let bindObject = {};
+bindObject.list = myList;
+ejs.renderFile("views/list-view.ejs", bindObject, function (err, result) {
+  if (err) console.log("ERROR - " + err);
+  console.log("----------------------------------------");
+  console.log(result);
+});
