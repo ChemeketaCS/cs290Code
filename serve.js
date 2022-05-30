e215bb5a4ff9471953403f7b47afd7a02a82125e
Andrew Scholer <ascholer@chemeketa.edu>
2022-05-30 10:12:57 -0700

    Add various notes to earlier weeks

--- serve.js
+++ serve.js
@@ -7,5 +7,12 @@ var serveIndex = require("serve-index");
 app.use("/", express.static("."), serveIndex(__dirname + "/"));
 
 app.listen(port, () => {
+  console.log("Running out of the root folder of cs290Code.");
+  console.log(
+    "If you are trying to run a node based sample (week 6+), make sure to run"
+  );
+  console.log(
+    "the start command from the folder of the project you want to run."
+  );
   console.log(`Example app listening at http://localhost:${port}`);
 });
