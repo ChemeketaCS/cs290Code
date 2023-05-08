f72cd81464600d25a4cedcf370915e4b10f45a87
Andrew Scholer <ascholer@chemeketa.edu>
2023-05-08 16:59:32 -0700

    Update npmInit Demo to use fetch 3.x

--- Week06/npmInitDemo/useFetch.js
+++ Week06/npmInitDemo/useFetch.js
@@ -2,38 +2,37 @@
 // Version 3 of node-fetch is ES6 style module - have to "import" not "require"
 //Need to install node-fetch with npm to have this work!
 //Install with "npm install node-fetch"
-//import fetch from 'node-fetch';
+import fetch from "node-fetch";
+//Must also make sure package.json specifies "type": "module"
 
-//Version 2 of node-fetch was a commonjs module - used this syntax:
-//Install with "npm install node-fetch@2.6"
-let fetch = require("node-fetch");
+//Using await syntax for fetch. Must put inside async function
+async function getData() {
+  try {
+    let response = await fetch(
+      "https://api.punkapi.com/v2/beers?per_page=4&yeast=34"
+    );
+    if (response.status !== 200)
+      throw response.status + " " + response.statusText;
+    let json = await response.json();
+    console.log(json);
+  } catch (except) {
+    console.log(except);
+  }
+}
+//Call the function
+getData();
 
-fetch("https://api.punkapi.com/v2/beers?per_page=4&yeast=05")
-  .then(function (response) {
-    if (response.status == 200) {
-      return response.json();
-    } else throw response.status + " " + response.statusText;
-  })
-  .then(function (json) {
-    let recipes = json;
-    console.log(recipes);
-  })
-  .catch(function (err) {
-    console.log("Fetch problem: " + err.message);
-  });
-
-
-// //Same idea, but implemented with async/await
-// async function main() {
-//   try {
-//     let response = await fetch(
-//       "https://api.punkapi.com/v2/beers?per_page=4&yeast=05"
-//     );
-//     let json = await response.json();
-//     console.log(json);
-//   } catch (except) {
-//     console.log(except);
-//   }
-// }
-
-// main();
+// //Do the same thing with .then syntax - can run as code not in a function
+// fetch("https://api.punkapi.com/v2/beers?per_page=4&yeast=34")
+//   .then(function (response) {
+//     if (response.status === 200) {
+//       return response.json();
+//     } else throw response.status + " " + response.statusText;
+//   })
+//   .then(function (json) {
+//     let recipes = json;
+//     console.log(recipes);
+//   })
+//   .catch(function (err) {
+//     console.log("Fetch problem: " + err.message);
+//   });
