4776feea9f3f77bed4dafbf08cd8ebc6121a0a94
Andrew Scholer <ascholer@chemeketa.edu>
2023-05-08 19:40:49 -0700

    rework npmInitDemo

---
+++ Week06/npmInitDemo/randWords.js
@@ -0,0 +1,12 @@
+//Use this package https://www.npmjs.com/package/random-words
+
+//Installed by doing:
+//npm install random-words
+
+//Bring in the library
+const randomWords = require('random-words');
+
+//Get some words
+let words = randomWords(20)
+
+console.log(words);
