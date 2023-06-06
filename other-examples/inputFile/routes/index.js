45bf85ec633b53ab282794f043d8bad59963a001
Andrew Scholer <ascholer@chemeketa.edu>
2023-06-06 08:52:50 -0700

    Add file input demo

---
+++ other-examples/inputFile/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res, next) {
+  console.log(req);
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
